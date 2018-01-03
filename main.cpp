5a30fda4ad4ce22a61c76170056a595f9afa5bf3
niliushall <1459775963@qq.com>
2018-01-04 00:39:53 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,61 @@
 #include<fstream>
 #include"map.h"
 
+void SchoolMap::print() const {
+    EdgeNode *p;
+    for(int i = 0; i < numVertexes; i++) {
+        p = Map[i].FirstEdge;
+        while(p) {
+            cout << Map[p->adjvex].name << endl;
+            p = p->next;
+        }
+        cout << endl;
+    }
+}
+
+void SchoolMap::menu() const {
+    int choice;
+
+    
+    do {
+        CLEAR;
+        cout << "============================================" << endl;
+        cout << "=========       校园导航系统        ==========" << endl;
+        cout << "============================================" << endl;
+        cout << "======    1. 输出校园平面图              ======" << endl;
+        cout << "======    2. 查询校园信息               ======" << endl;
+        cout << "======    3. 查询两地间所有简单路径       ======" << endl;
+        cout << "======    4. 查询两地间中转次数最少路径    ======" << endl;
+        cout << "======    5. 查询两地间最佳访问路径       ======" << endl;
+        cout << "======    6. 设计最佳布网方案            ======" << endl;
+        cout << "======    0. 退出                      ======" << endl;
+        cout << "============================================" << endl << endl;
+        cout << "请输入需要选择的数字：\n";
+
+        cin >> choice;
+
+        switch(choice) {
+            case 0: return;
+            // case 1: ;
+            case 2: info(); WAIT; break;
+            case 3: SchoolMap::allPath(); WAIT; break;
+            case 4: SchoolMap::shortestPath(); WAIT; break;
+            case 5: SchoolMap::bestPath(); WAIT; break;
+            // case 6: SchoolMap::bestMap(); WAIT; break;
+            // default:
+                cout << "数字超出范围，按任意键返回...\n";
+                fflush(stdin);
+                cin.get();
+                break;
+
+        }
+    }while(choice);
+    
+
+ 
+
+}
+
 int SchoolMap::findVertexNum(const string & str) const {
     int i;
     for(i = 0; i < numVertexes; i++) {
@@ -8,7 +63,9 @@ int SchoolMap::findVertexNum(const string & str) const {
             return i;
         }
     }
-    return 0;
+    
+    cout << "未找到该地点 - " << str << endl;
+    return -1;
 }
 
 void SchoolMap::CreateMapFromFile() {
@@ -64,23 +121,180 @@ void SchoolMap::CreateMapFromFile() {
 }
 
 int SchoolMap::info() const {
-    string filename;
+    string filename, t("information/");
     cout << "请输入要查询信息的地点： ";
     cin >> filename;
+    filename = t + filename;
 
-    istream fin;
+    ifstream fin;
     fin.open(filename);
     if(!fin.is_open()) {
         cout << "打开文件 " << filename << " 错误\n";
         return 0;
     }
 
-    fin >> 
+    char ch;
+    while(fin.get(ch)) {
+        cout << ch;
+    }
+
+    fin.close();
+}
+
+void SchoolMap::allPath() const {
+    string str1, str2;
+    int start, end;
+    vector<bool> visited(MAXVEX, false);
+    vector<int> path;    
+
+    cout << "请输入路径的起点：\n";
+    cin >> str1;
+    cout << "请输入路径的终点：\n";
+    cin >> str2;
+
+    start = findVertexNum(str1);
+    end = findVertexNum(str2);
+
+    //地点输入错误
+    if(start == -1 || end == -1) {
+        return;
+    }
+
+    dfs(start, end, visited, path);
+}
+
+void SchoolMap::dfs(const int start, const int end, vector<bool> visited, vector<int> path) const {
+    EdgeNode *p;
+    p = Map[start].FirstEdge;
+    visited[start] = true;
+    path.push_back(start);  //将该函数的起点放入数组    
+
+    while(p) {
+        if(p->adjvex != end && !visited[p->adjvex]) {
+            dfs(p->adjvex, end, visited, path);
+        } else if(p->adjvex == end) {
+            for(vector<int>::iterator it = path.begin(); it != path.end(); it++) {
+                cout << Map[*it].name << " --> ";
+            }
+            cout << Map[end].name << endl;
+        }
+        p = p->next;
+    }
+    visited[start] = false;
+    path.pop_back();
+}
+
+void SchoolMap::shortestPath() const {
+    string str1, str2;
+    int start, end, min = 99999999, len = 1;
+    vector<bool> visited(MAXVEX, false);
+    vector<int> path;
+    vector<int> tmp;
+
+    cout << "请输入路径的起点：\n";
+    cin >> str1;
+    cout << "请输入路径的终点：\n";
+    cin >> str2;
+
+    start = findVertexNum(str1);
+    end = findVertexNum(str2);
+
+    //地点输入错误
+    if(start == -1 || end == -1) {
+        cout << "所输入地点不存在\n";
+        return;
+    }
+
+    shortestPathDfs(start, end, visited, path, tmp, min, len);//dfs寻找最短路径
+
+    cout << "最短路径（中转次数最少）为：\n";
+    for(vector<int>::iterator it = path.begin(); it != path.end(); it++) {
+        cout << Map[*it].name << " --> ";
+    }
+    cout << Map[end].name << endl;
+}
+
+void SchoolMap::shortestPathDfs(const int start, const int end, vector<bool> visited, vector<int> & path, vector<int> & tmp, int & min, int len) const {
+    EdgeNode *p;
+    p = Map[start].FirstEdge;
+    visited[start] = true;
+    tmp.push_back(start);
+// cout << Map[start].name << ' ' << Map[p->adjvex].name << ' ' << Map[end].name << endl;
+
+    while(p) {
+        if(p->adjvex != end && !visited[p->adjvex]) {
+            shortestPathDfs(p->adjvex, end, visited, path, tmp, min, len+1);
+        } else if(p->adjvex == end) {
+            if(min > len) {
+                min = len;
+                path = tmp;
+            }
+        }
+        p = p->next;
+    }
+    visited[start] = false;
+    tmp.pop_back();
+}
+
+void SchoolMap::bestPath() const {
+    string str1, str2;
+    int start, end, min = 9999999, len = 0;
+    vector<bool> visited(MAXVEX, false);
+    vector<int> path;
+    vector<int> tmp;
+
+    cout << "请输入路径的起点：\n";
+    cin >> str1;
+    cout << "请输入路径的终点：\n";
+    cin >> str2;
+
+    start = findVertexNum(str1);
+    end = findVertexNum(str2);
+
+    //地点输入错误
+    if(start == -1 || end == -1) {
+        cout << "所输入地点不存在\n";
+        return;
+    }
+
+    bestPathDfs(start, end, visited, path, tmp, min, len);
+
+    cout << "最短路径(距离最小)为：\n";
+    for(vector<int>::iterator it = path.begin(); it != path.end(); it++) {
+        cout << Map[*it].name << " --> ";
+    }
+    cout << Map[end].name << endl;
+    cout << "最短路径长度为： " << min << endl;
+}
+
+void SchoolMap::bestPathDfs(const int start, const int end, vector<bool> visited, vector<int> & path, vector<int> tmp, int & min, int len) const {
+    EdgeNode *p;
+    p = Map[start].FirstEdge;
+    visited[start] = true;
+    tmp.push_back(start);
+
+    while(p) {
+// cout << Map[start].name << ' ' << Map[p->adjvex].name << ' ' << Map[end].name << endl;
+        
+        if(p->adjvex != end && !visited[p->adjvex]) {
+            bestPathDfs(p->adjvex, end, visited, path, tmp, min, len+p->length);
+        } else if(p->adjvex == end) {
+            if(min > len) {
+                min = len;
+                path = tmp;
+            }
+        }
+        p = p->next;
+    }
+    visited[start] = false;
+    tmp.pop_back();
 }
 
 int main() {
     SchoolMap map;
     map.CreateMapFromFile();
+
+    map.menu();
 
     return 0;
 }
--- map.h
+++ map.h
@@ -1,8 +1,11 @@
 #include<iostream>
+#include<cstdio>
 #include<vector>
 #include<string>
 using namespace std;
 
+#define CLEAR system("clear");
+#define WAIT {cin.get(); cin.get();};
 const int MAXVEX = 20;
 
 struct EdgeNode {
@@ -23,7 +26,16 @@ private:
     int numEdges;  //边个数
 public:
     SchoolMap() {numVertexes = numEdges = 0;};
+    void menu() const;
     void CreateMapFromFile();  //创建邻接表
     int findVertexNum(const string & str) const;  //寻找结点下标
     int info() const;
+    void allPath() const;  //所有简单路径
+    void dfs(const int start, const int end, vector<bool> visited, vector<int> path) const;
+    void shortestPath() const;  //中转次数最少路径
+    void shortestPathDfs(const int start, const int end, vector<bool> visited, vector<int> & path, vector<int> & tmp, int & max, int len) const;
+    void bestPath() const;  //最短路径
+    void bestPathDfs(const int start, const int end, vector<bool> visited, vector<int> & path, vector<int> tmp, int & max, int len) const;
+    void bestMap() const;  //最佳布网方案
+    void print() const; 
 };
